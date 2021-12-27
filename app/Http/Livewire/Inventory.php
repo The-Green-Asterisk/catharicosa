fedf42ab9ec89d004086259d817756dd2e125045
Steve <live.remix@gmail.com>
2021-12-27 01:19:20 -0500

    Implemented D&D Beyond inventory

--- app/Http/Livewire/Inventory.php
+++ app/Http/Livewire/Inventory.php
@@ -8,19 +8,49 @@ class Inventory extends Component
 {
     public $inv;
 
+    public $sheetNumber;
+
+    public $error;
+
+    public $name;
+
+    public function mount()
+    {
+        $this->getInv();
+    }
+
     public function getInv()
     {
-        $char_file = file_get_contents('https://character-service.dndbeyond.com/character/v3/character/11600727');
-        $char = json_decode($char_file, false);
-        $this->inv = $char->data->inventory;
+        if (isset($this->sheetNumber))
+        {
+            if ($this->sheetNumber === "")
+            {
+                $this->error = "";
+                return;
+            }
+            $fileURL = 'https://character-service.dndbeyond.com/character/v3/character/' . $this->sheetNumber;
+            if (preg_match('/^[0-9]{8}$/', $this->sheetNumber))
+            {
+                $charFile = file_get_contents($fileURL);
+                $char = json_decode($charFile, false);
+            }
+            isset($char->success)
+                ? $this->inv = $char->data->inventory
+                : $this->error = "That's not a valid number";
+            isset($char->data->name)
+                ? $this->name = $char->data->name
+                : $this->name = null;
+        }else{
+            $this->sheetNumber = "";
+            $this->error = "";
+        }
     }
 
     public function render()
     {
-        $this->getInv();
-
         return view('livewire.inventory', [
-            'inv' => $this->inv
+            'inv' => $this->inv,
+            'name' => $this->name
         ]);
     }
 }
--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -5,10 +5,20 @@
             line-height: 1.25rem;
         }
     </style>
-    @foreach ($inv as $item)
-        <div class="mb-4 inv">
-            <p class="font-bold">{{ $item->definition->name }}</p>
-            {!! $item->definition->description !!}
-        </div>
-    @endforeach
+    <input class="border w-full text-sm rounded" wire:model="sheetNumber" wire:input="getInv()" type="text" placeholder="Paste the number from your D&D Beyond character sheet's URL" />
+    @if (isset($inv))
+        <h1 class="text-lg underline font-bold decoration-4">{{ $name ? $name . "'s inventory:" : "" }}</h1>
+        <p class="text-sm mb-4">{!! $name ? "Manage " . $name . "'s inventory on <a class='hover:text-blue-800' href='https://www.dndbeyond.com/my-characters'>D&D Beyond.com</a>" : "" !!}</p>
+        @foreach ($inv as $item)
+            <div class="mb-4 inv">
+                @if (isset($item->definition->name))
+                    <p class="font-bold">{{ $item->definition->name }}</p>
+                    {!! $item->definition->description !!}
+                @else
+                    <p class="text-sm text-red-800">{{ $error }}</p>
+                    @break
+                @endif
+            </div>
+        @endforeach
+    @endif
 </div>
