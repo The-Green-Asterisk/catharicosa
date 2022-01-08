f6a371aba00cea0c8f07b20494a78452ddb3c301
Steve <live.remix@gmail.com>
2022-01-08 11:46:17 -0500

    Add favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        
+
         <title>Catharicosa Notes</title>
 
+        <link rel="icon" type="image/png" href="/favicon.png">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
