5749493b1b5e1729bb42adecd59b77b55983bcf4
Steve <live.remix@gmail.com>
2022-01-14 10:33:11 -0500

    set guide for note creation process

--- resources/views/livewire/new-note.blade.php
+++ resources/views/livewire/new-note.blade.php
@@ -5,9 +5,17 @@
             <input type="text" wire:model="title" class="shadow-inner outline-gray-200 w-full font-bold rounded" value="Notes for {{ now()->tz(auth()->user()->timezone)->toDayDateTimeString() }}" />
             <textarea wire:model="body" wire:keydown.ctrl.enter="submit" class="shadow-inner w-full h-48 outline-gray-200 rounded" placeholder="Along our way we discovered a small town with a lively tavern. Perhaps TOO lively..."></textarea>
             <div class="flex">
-                <div class="flex items-center text-sm italic">Ctrl+Enter will save the note. Note must be saved before notelettes can be created.</div>
+                <div class="flex text-left text-xs italic">
+                    <ul  class="list-disc list-inside">
+                        Things to keep in mind:
+                        <li>Ctrl+Enter will save the note.</li>
+                        <li>Note must be saved before notelettes can be created.</li>
+                        <li>Don't worry, you can continue your note after it's saved!</li>
+                        <li>Notelettes can be created at any point after the initial save by selecting a portion of the note, right-clicking, and selecting an association.</li>
+                    </ul>
+                    </div>
                 <div class="grow"></div>
-                <x-form-button>Save</x-form-button>
+                <x-form-button class="my-auto">Save</x-form-button>
             </div>
         </div>
     </form>
