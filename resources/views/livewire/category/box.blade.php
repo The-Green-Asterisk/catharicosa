8ad2c62e3262ff77eea4c65e8c370a9ff13ce84e
Steve <live.remix@gmail.com>
2022-01-19 12:36:44 -0500

    updated item box with no horizontal scrollbar

--- resources/views/livewire/category/box.blade.php
+++ resources/views/livewire/category/box.blade.php
@@ -3,7 +3,7 @@
         <div x-init="$wire.emit('showModal', '{{ $catName }}', {{ $category->id }})"></div>
     @endif
     <div class="shadow flex flex-col flex-none">
-        <div class="rounded-t border border-slate-500 bg-slate-200 border-b-none font-bold p-1 h-8 w-48 flex overflow-x-auto overflow-y-hidden">
+        <div class="rounded-t border border-slate-500 bg-slate-200 border-b-none font-bold p-1 h-8 w-48 flex overflow-x-auto overflow-y-hidden no-scrollbar">
             <div class="flex-shrink-0">{{ $category->title ?? $category->name }}</div>
         </div>
         <div class="rounded-b border border-slate-500 border-t-none p-2 h-40 w-48 overflow-y-auto">
