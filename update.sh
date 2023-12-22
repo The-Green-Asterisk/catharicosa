8678b6c6d8f73f0561eff8e54695f509fa448eb2
Steve Beaudry <live.remix@gmail.com>
2023-12-22 18:23:38 -0500

    another update

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+echo $(date +"%Y-%m-%d %T")
 cd /usr/local/var/www/catharicosa/notes
 git pull origin main
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
@@ -5,3 +6,4 @@ php artisan migrate --force
 npm run prod
 php artisan cache:clear
 php artisan route:cache
+echo $(date +"%Y-%m-%d %T")
