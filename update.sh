679adf6da3d22921cc7197071e820e42f6726ee2
Steve Beaudry <live.remix@gmail.com>
2023-12-21 21:27:35 -0500

    update to the update

--- update.sh
+++ update.sh
@@ -3,3 +3,5 @@ git pull origin main
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 php artisan migrate --force
 npm run prod
+php artisan cache:clear
+php artisan route:cache
